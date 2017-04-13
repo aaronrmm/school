0291861b9203864cbe6d76c652f28db5646a65a9
Aaron <Aaron@DESKTOP-RQUV63B>
2017-04-12 22:16:23 -0400

    added implied conflict test

---
+++ src/Test8.java
@@ -0,0 +1,17 @@
+
+
+
+
+public class Test8 {
+
+	public static void main(String[] args) {
+		TMS tms = new TMS();
+		
+		
+		tms.add_sentence("A->B");
+		tms.add_sentence("A->-B");
+		tms.add_sentence("A");
+		System.out.println(tms.getState());
+	}
+
+}
